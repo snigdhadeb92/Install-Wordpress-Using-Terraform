b14941611a351bc7234e04820fa05bde502ca5f4
Snigdhadeb Ash <83946236+snigdhadeb92@users.noreply.github.com>
2022-05-06 11:51:17 +0530

    Delete s3 directory

--- s3/main.tf
+++
@@ -1,9 +0,0 @@
-############## Lunch S3 Bucket ################
-resource "aws_s3_bucket" "bucket" {
-  #bucket = "my-test-bucket"
-
-  tags = {
-    Name        = var.bucket_name
-    Environment = "Dev"
-  }
-}
--- s3/variable.tf
+++
@@ -1,5 +0,0 @@
-variable "bucket_name" {
-  description = "Bucket name"
-  type        = string
-  default     = "snigdhadeb2023"
-}
